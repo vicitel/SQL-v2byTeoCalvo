f6209d65d0bda28edffa28a5e8e6e61421fbbdf7
letícia ghirotto <ghirotto@outlook.com>
2023-04-02 17:11:01 -0300

    primeiro commit

---
+++ select.sql
@@ -0,0 +1,8 @@
+select seller_id,
+        seller_state
+
+from tb_sellers
+
+-- comando 'select' acessa os campos. quando asterisco (*) acessa todos campos
+
+-- campos = colunas. (ex: seller_city)
---
+++ where.sql
@@ -0,0 +1,7 @@
+select *
+
+from tb_sellers as t1
+
+where seller_state = 'SP'
+
+-- filtro
